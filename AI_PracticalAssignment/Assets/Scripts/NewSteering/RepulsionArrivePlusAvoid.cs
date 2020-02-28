7389538a6e728f4895912df7cf99fae8749f49ae
Jordi Alba <44704875+jalba002@users.noreply.github.com>
2020-02-29 00:02:43 +0100

    Zombie flare surrounding.

--- AI_PracticalAssignment/Assets/Scripts/NewSteering/RepulsionArrivePlusAvoid.cs
+++ AI_PracticalAssignment/Assets/Scripts/NewSteering/RepulsionArrivePlusAvoid.cs
@@ -5,11 +5,11 @@ namespace Steerings
     // combine Keep Position + Linear Repulsion
     public class RepulsionArrivePlusAvoid : SteeringBehaviour
     {
-        public RotationalPolicy rotationalPolicy = RotationalPolicy.LWYGI;
+        public RotationalPolicy rotationalPolicy = RotationalPolicy.FTI;
 
         // parameters required by KeepPosition
-        public float closeEnoughRadius = 5f;  // also know as targetRadius
-        public float slowDownRadius = 20f;    // if inside this radius, slow down
+        public float closeEnoughRadius = 5f; // also know as targetRadius
+        public float slowDownRadius = 20f; // if inside this radius, slow down
         public float timeToDesiredSpeed = 0.1f;
 
         public GameObject target;
@@ -18,27 +18,37 @@ namespace Steerings
         public string idTag = "REPULSIVE";
         public float repulsionThreshold = 8.0f;
 
+        [Range(0f, 1f)] public float repulsionWeight = 0.5f;
+
         public override SteeringOutput GetSteering()
         {
             // no KS? get it
             if (this.ownKS == null) this.ownKS = GetComponent<KinematicState>();
 
-            SteeringOutput result = RepulsionArrivePlusAvoid.GetSteering(this.ownKS, this.target, this.closeEnoughRadius, this.slowDownRadius, this.timeToDesiredSpeed, this.idTag, this.repulsionThreshold);
+            SteeringOutput result = RepulsionArrivePlusAvoid.GetSteering(this.ownKS, this.target,
+                this.closeEnoughRadius, this.slowDownRadius, this.timeToDesiredSpeed, this.idTag,
+                this.repulsionThreshold, repulsionWeight);
             base.applyRotationalPolicy(rotationalPolicy, result, target);
             return result;
         }
 
-        public static SteeringOutput GetSteering(KinematicState ownKS, GameObject target, float distance = 10.0f, float slowRadius = 20.0f, float disredSpeed = 0.1f, string tag = "REPULSIVE", float repulsionTh = 8.0f)
+        public static SteeringOutput GetSteering(KinematicState ownKS, GameObject target, float distance = 10.0f,
+            float slowRadius = 20.0f, float disredSpeed = 0.1f, string tag = "REPULSIVE", float repulsionTh = 8.0f, float repulsionWeight = 0.5f)
         {
-            SteeringOutput so = LinearRepulsion.GetSteering(ownKS, tag, repulsionTh);
-
+            SteeringOutput linearRepulsion = LinearRepulsion.GetSteering(ownKS, tag, repulsionTh);
+            SteeringOutput arrive = Arrive.GetSteering(ownKS, target, distance, disredSpeed);
             // Give priority to linear repulsion
             // (if linear repulsion is not SteeringBehaviour.NULL_STEERING return linear repulsion
-            if (so != NULL_STEERING)
-                return so;
+            if (linearRepulsion == NULL_STEERING)
+                return arrive;
+
+            arrive.linearAcceleration = arrive.linearAcceleration * (1 - repulsionWeight) +
+                                        linearRepulsion.linearAcceleration * repulsionWeight;
+            arrive.angularAcceleration = arrive.angularAcceleration * (1 - repulsionWeight) +
+                                         linearRepulsion.angularAcceleration * repulsionWeight;
 
             // else return Keep Position
-            return Arrive.GetSteering(ownKS, target, distance, disredSpeed);
+            return arrive;
         }
     }
 }
